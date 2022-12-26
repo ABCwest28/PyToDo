90bcb0eba75ab7e9fd88596a5f3d28c214883341
ABCwest28 <ABCwest28@gmail.com>
2022-12-26 21:59:07 +0500

    QMainWindow: 1. Окошко центрировано (setWindowToCenter) 2. Подтверждение выхода (setWindowToCenter)

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-def print_hi(name):
-    print(f'Hi,{name}')
+import sys
+from PyQt5.QtWidgets import *
+
+
+class WidgetToDo(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.resize(500, 500)
+        self.setWindowTitle('PyToDo')
+        self.setWindowToCenter()
+        self.show()
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Подтверждение',
+                "Подтвердите выход", QMessageBox.Yes |
+                QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+    def setWindowToCenter(self):
+        my_frame_geom = self.frameGeometry()
+        screen_center = QDesktopWidget().availableGeometry().center()
+        my_frame_geom.moveCenter(screen_center)
+        self.move(my_frame_geom.topLeft())
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app = QApplication(sys.argv)
+    ex = WidgetToDo()
+    sys.exit(app.exec_())
