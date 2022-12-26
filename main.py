995215a91f1975a72b9facaaa4a8409040d58207
ABCwest28 <ABCwest28@gmail.com>
2022-12-26 23:40:48 +0500

    list_widget addRow()

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ class WidgetToDo(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
+        self.list_widget = QListWidget()
 
     def initUI(self):
         self.resize(500, 500)
+        self.setObjectName("WidgetToDo")
         self.setWindowTitle('PyToDo')
         self.setWindowToCenter()
+        self.statusBar().showMessage("Started")
         self.show()
 
     def closeEvent(self, event):
@@ -27,6 +30,18 @@ class WidgetToDo(QMainWindow):
         screen_center = QDesktopWidget().availableGeometry().center()
         my_frame_geom.moveCenter(screen_center)
         self.move(my_frame_geom.topLeft())
+
+    def newRow(self):
+        new_check_box = QCheckBox()
+        new_line_edit = QLineEdit()
+        new_button = QPushButton()
+
+        new_h_layout = QHBoxLayout()
+        new_h_layout.addWidget(new_check_box)
+        new_h_layout.addWidget(new_line_edit)
+        new_h_layout.addWidget(new_button)
+
+        self.list_widget.addItem(new_h_layout)
 
 
 if __name__ == '__main__':
