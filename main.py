b29fcf753896812f025a53fb83e99f394159faa1
ABCwest28 <ABCwest28@gmail.com>
2022-12-27 19:00:44 +0500

    Виджеты размещены в Layout-ах

--- main.py
+++ main.py
@@ -5,12 +5,33 @@ from PyQt5.QtWidgets import *
 class WidgetToDo(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.wrapper = QWidget(self)
         self.initUI()
 
     def initUI(self):
         self.resize(500, 500)
         self.setWindowTitle('PyToDo')
+        self.statusBar().showMessage("Started")
         self.setWindowToCenter()
+        self.vbox = QVBoxLayout(self.wrapper)
+        self.vbox.setSpacing(10)
+        self.h1box = QHBoxLayout(self.wrapper)
+        table = QTableWidget(self.wrapper)
+        table.resize(480, 400)
+        self.vbox.addWidget(table)
+        self.vbox.addLayout(self.h1box)
+        task_line = QLineEdit(self)
+        task_line.resize(200, 30)
+        self.h1box.addWidget(task_line)
+        task_date = QDateEdit(self)
+        task_date.resize(140, 30)
+        self.h1box.addWidget(task_date)
+        task_btn = QPushButton(self)
+        task_btn.resize(20, 30)
+        self.h1box.addWidget(task_btn)
+        self.setLayout(self.vbox)
+
+        self.setCentralWidget(self.wrapper)
         self.show()
 
     def closeEvent(self, event):
